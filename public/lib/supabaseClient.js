cdc3b3b69fcc40385544feac2253f2213a4cc019
kubiasofficial <suslik.kuba@seznam.cz>
2025-07-08 19:28:19 +0200

    feat: add Supabase client initialization for database interactions

---
+++ public/lib/supabaseClient.js
@@ -0,0 +1,7 @@
+// lib/supabaseClient.js
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://mdlvszaufcchjnlvxlgo.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
