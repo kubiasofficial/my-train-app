50f5309d5ace21e2a091a4c10a4bb1899425bc1a
kubiasofficial <suslik.kuba@seznam.cz>
2025-07-09 16:20:56 +0200

    fix: remove redundant import statement for supabase in strojvedouci.html

--- public/lib/supabaseClient.js
+++ public/lib/supabaseClient.js
@@ -5,3 +5,5 @@ const supabaseUrl = 'https://mdlvszaufcchjnlvxlgo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
+import { supabase } from '/lib/supabaseClient.js';
